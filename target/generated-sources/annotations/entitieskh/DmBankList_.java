95b8a5fb2c43adb8519660db7627e9f8f2eaf076
Nguyen Huu Tam Chau <chaunht>
2017-12-23 10:31:11 +0700

    v1

--- target/generated-sources/annotations/entitieskh/DmBankList_.java
+++ target/generated-sources/annotations/entitieskh/DmBankList_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-23T09:53:31")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-23T10:30:16")
 @StaticMetamodel(DmBankList.class)
 public class DmBankList_ { 
 
