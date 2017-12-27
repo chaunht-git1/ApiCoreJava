fad9638122640b2cf7776d1032a18aadcb854a1f
Nguyen Huu Tam Chau <chaunht>
2017-12-27 10:19:00 +0700

    Revert "26122017-Capnhatkh"
    This reverts commit afdd682c0a0a5552fe37a1f5f78f6febc9a8cfe1.

--- target/generated-sources/annotations/entitieskhout/VwDmQuanlydotimagectietWeb_.java
+++ target/generated-sources/annotations/entitieskhout/VwDmQuanlydotimagectietWeb_.java
@@ -6,7 +6,11 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
+<<<<<<< HEAD
 @Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-27T09:47:26")
+=======
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-25T20:42:49")
+>>>>>>> parent of afdd682... 26122017-Capnhatkh
 @StaticMetamodel(VwDmQuanlydotimagectietWeb.class)
 public class VwDmQuanlydotimagectietWeb_ { 
 
--- target/generated-sources/annotations/entitieskhout/VwTondongNvAllWeb_.java
+++ target/generated-sources/annotations/entitieskhout/VwTondongNvAllWeb_.java
@@ -7,7 +7,11 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
+<<<<<<< HEAD
 @Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-27T09:47:26")
+=======
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-25T20:42:49")
+>>>>>>> parent of afdd682... 26122017-Capnhatkh
 @StaticMetamodel(VwTondongNvAllWeb.class)
 public class VwTondongNvAllWeb_ { 
 
